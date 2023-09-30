0496d5f842ea910b8b24682c63634aba286bc9c9
shilpajpatil <40339744+shilpajpatil@users.noreply.github.com>
2023-09-30 08:23:10 -0700

    Add files via upload
    if user wants to add multiple tags using react.

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
-const heading = React.createElement("h1" , {}, "hello world from React!!");
+
+const heading = React.createElement("div" , {id:"parent"},"Hello",  
+                [React.createElement("h1",{id:"child1"},"inside tag"),
+                React.createElement("h2",{id:"child2"},"inside h2")]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
